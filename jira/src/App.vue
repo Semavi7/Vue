e51dc67f026ef889f1e5404e6e723b046bcbe916
Mehmet Burçhan Gürses <semavi1151@gmail.com>
2025-07-07 18:05:07 +0300

    Vue

---
+++ jira/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <nav>
+      <router-link to="/">Home</router-link>
+    </nav>
+    <router-view/>
+  </div>
+</template>
+
+<style>
+*{
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+body{
+  background: ghostwhite;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
---
+++ jira/src/views/HomeView.vue
@@ -0,0 +1,41 @@
+<template>
+  <div v-if="tasks.length">
+    <div v-for="task in tasks" :key="task.id">
+      <SingleTask :task="task" @delete="handleDelete" @complete="handleComplete"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import SingleTask from '@/components/SingleTask.vue';
+
+
+
+export default {
+  name: 'HomeView',
+  components: {
+    SingleTask
+  },
+  data() {
+    return {
+      tasks:[]
+    }
+  },
+  mounted() {
+    fetch("http://localhost:3000/tasks").then(res=>res.json()).then(data=>this.tasks=data).catch(err=>console.log(err.message))
+  },
+  methods: {
+    handleDelete(id){
+      this.tasks = this.tasks.filter(task => {
+        return task.id !== id
+      })
+    },
+    handleComplete(id){
+      let myTask = this.tasks.find(task=>{
+        return task.id === id
+      })
+      myTask.complete = !myTask.complete
+    }
+  },
+}
+</script>
